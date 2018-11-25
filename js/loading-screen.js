5515245d514d9eff83e64745c8d31164940cfc91
Mike Taylor <mctaylor17@gmail.com>
2018-11-25 13:35:07 -0800

    Adds loading screen

---
+++ js/loading-screen.js
@@ -0,0 +1,45 @@
+  (function($) {
+    $("main").hide();
+    var particles = Particles.init({
+      selector: '.particles'
+    });
+    
+    var interval = setInterval(loadingMessage,1000);
+    
+    var $dots = $(".loading-screen .dots");
+    var count = 0;
+    function loadingMessage() {
+      var dots;
+      switch (count) {
+        case 0:
+          dots = "&nbsp;&nbsp;&nbsp;";
+          break;
+        case 1:
+          dots = ".&nbsp;&nbsp;";
+          break;
+        case 2:
+          dots = "..&nbsp;";
+          break;
+        case 3:
+          dots = "...";
+          break;
+      }
+            
+      count++;
+      
+      if(count === 4) {
+        count = 0;
+      }
+      
+      $dots.html(dots);
+    }
+    
+    $(window).on("load",removeLoadingScreen);
+
+    function removeLoadingScreen() {
+      $("main").fadeIn();
+      $(".loading-screen").fadeOut(function() {
+        particles.destroy();
+      });
+    }
+  }(jQuery));
